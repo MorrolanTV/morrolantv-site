50299a1d957d82ac7d9b4c2d02217a502237ba53
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2020-09-06 21:09:40 +0200

    add twitch player component

---
+++ components/TwitchPlayer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="tw-wrapper">
+    <iframe
+      src="https://player.twitch.tv/?channel=morrolantv&parent=morrolantv-dev.herokuapp.com"
+      frameborder="0"
+      allowfullscreen="false"
+      scrolling="no"
+      height="100%"
+      width="100%"
+    ></iframe>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <main>
     <section class="hero w-100">
-      <div class="image" :style="{ backgroundImage: `url(${backgroundUrl})` }">
-        <video></video>
+      <div class="image-wrapper">
+        <div
+          class="image"
+          :style="{ backgroundImage: `url(${backgroundUrl})` }"
+        >
+          <video></video>
+        </div>
       </div>
-      <div>
-        <!--<iframe
-          src="https://player.twitch.tv/?channel=morrolantv&parent=morrolantv-dev.herokuapp.com"
-          frameborder="0"
-          allowfullscreen="false"
-          scrolling="no"
-          height="100%"
-          width="100%"
-        ></iframe>-->
+      <div class="showcase-wrapper">
+        <div class="showcase-element">
+          <TwitchPlayer />
+        </div>
       </div>
     </section>
     <Toollist />
@@ -32,16 +32,19 @@ export default {
 .hero {
   max-width: 100%;
   flex-direction: row;
-  padding: 10vh 0;
   max-height: none;
   align-items: center;
   position: relative;
-  overflow: hidden;
   width: 100%;
   height: calc(65vh);
   min-height: 300px;
-  margin-bottom: 50px;
+  margin-bottom: 100px;
 
+  .image-wrapper {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
   .image {
     position: absolute;
     top: 0;
@@ -52,5 +55,19 @@ export default {
     background-position: center 15%;
     background-size: cover;
   }
+}
+
+.showcase-wrapper {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  transform: translateY(50%);
+  padding: 0 100px;
+}
+
+.showcase-element {
+  flex-basis: 350px;
 }
 </style>
