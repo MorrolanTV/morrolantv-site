3f439bf3c287d60a84c79eff563e75cdd6539495
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2020-10-10 21:54:21 +0200

    Fix typo in title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'Morrolan Tv',
+    title: 'Morrolan TV',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
