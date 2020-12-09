3ce611f01bd3aef6b791ed2f4143f9f85ccd7542
Sirfilior <66057362+Sirfilior@users.noreply.github.com>
2020-12-09 22:22:10 +0100

    add chilly

--- pages/About.vue
+++ pages/About.vue
@@ -20,19 +20,35 @@
             <p class="mt-5">
               Focus by Advanced is 0 calorie, sugar free energy drink that I use
               every day. There are three main reasons why I use Focus instead of
-              the dozens of other energy drink options: 1) Drinking a ton of
-              caffeine often made me jittery or anxious. Focus contains
-              L-Theanine, an amino acid found in tea, which combines with
-              caffeine to give you the same energy and alertness but without any
-              jitters. HUGE difference, I will never drink an energy drink
-              without L-Theanine ever again. 2) Most energy drinks are a
-              combination of caffeine & vitamins, which is fine. Focus takes it
-              a step further with a blend of nootropics which have been
-              clinically proven to do things like boost memory, improve reaction
-              speed, and help your eyes recover from long periods of staring at
-              a screen. 3) Lots of these drinks have chalky textures, especially
-              once you get to the bottom of the shaker. Focus is less bulky and
-              blends nicely into water without that chalkiness.
+              the dozens of other energy drink options:<br />
+              1) Drinking a ton of caffeine often made me jittery or anxious.
+              Focus contains L-Theanine, an amino acid found in tea, which
+              combines with caffeine to give you the same energy and alertness
+              but without any jitters. HUGE difference, I will never drink an
+              energy drink without L-Theanine ever again.<br />
+              2) Most energy drinks are a combination of caffeine & vitamins,
+              which is fine. Focus takes it a step further with a blend of
+              nootropics which have been clinically proven to do things like
+              boost memory, improve reaction speed, and help your eyes recover
+              from long periods of staring at a screen.<br />
+              3) Lots of these drinks have chalky textures, especially once you
+              get to the bottom of the shaker. Focus is less bulky and blends
+              nicely into water without that chalkiness.
+            </p>
+          </div>
+          <div class="mb-5">
+            <a target="_blank" href="https://advanced.gg/?ref=MORROLANTV"
+              ><img
+                style="max-width: 320px"
+                :src="require('~/assets/img/chill.jpg')"
+            /></a>
+            <p class="mt-5">
+              Advanced also has a caffeine-free CBD drink called Chill which
+              took me totally by surprise. I was never much of a CBD person, but
+              they sent me some to try, and I was amazed at how well it works to
+              help me de-stress and relax at the end of a long day. Don't just
+              take my word for it - ask in Twitch chat or Discord, and you will
+              find lots of people with great things to say about this product.
             </p>
           </div>
           <div class="mb-5">
